d3a1d604c7a3ae578b393cece4c9a1147fb7985a
lughjw <lughjw@gmail.com>
2020-01-29 17:49:54 -0600

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    #The [] in the regex lets it look for individual characters and the + means ore more sequential instances of
+    word_array = self.split(/[?.!]+/)
+    #binding.pry
+    word_array.size
   end
 end
